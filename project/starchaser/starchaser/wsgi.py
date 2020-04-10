930eae43d1712a4684ca59ee4ca7cc72066763dc
Chris Winsor <cwinsor@gmail.com>
2020-04-09 22:58:24 -0400

    update wsgi.py

--- project/starchaser/starchaser/wsgi.py
+++ project/starchaser/starchaser/wsgi.py
@@ -11,6 +11,19 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import sys
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'starchaser.settings')
+
+try:
+        application = get_wsgi_application()
+            print 'WSGI without exception'
+except Exception:
+        print 'handling WSGI exception'
+            # Error loading applications
+                if 'mod_wsgi' in sys.modules:
+                            traceback.print_exc()
+                                    os.kill(os.getpid(), signal.SIGINT)
+                                            time.sleep(2.5)
 
 application = get_wsgi_application()
