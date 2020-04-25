af183a260aa7f3ff907955cc6dc7f23fbea6b2dc
Chris Winsor <cwinsor@gmail.com>
2020-04-25 04:20:37 -0400

    update aws fix superuser

--- project/starchaser/starchaser/management/commands/createsu.py
+++ project/starchaser/starchaser/management/commands/createsu.py
@@ -8,3 +8,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username="cwinsor").exists():
             User.objects.create_superuser("cwinsor", "cwinsor@gmail.com", "abc123")
+        if not User.objects.filter(username="cwinsor2").exists():
+            User.objects.create_superuser("cwinsor2", "cwinsor@gmail.com", "abc123")
