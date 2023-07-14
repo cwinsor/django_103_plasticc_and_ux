989eb69ac451406a8e46dad1ece72a9dcdc56ad3
Ubuntu <ubuntu@ip-172-31-11-218.us-east-2.compute.internal>
2023-07-14 16:44:09 +0000

    cleanup setup_linux.sh

--- project/setup_linux.sh
+++ project/setup_linux.sh
@@ -3,17 +3,92 @@
 # This is setup script and instructions to deploy StarChaser on AWS including the PostgreSQL data subset
 # It is intended to be run on the EC2
 
-export PYTHONPATH=$(pwd):$(pwd)/starchaser:$(pwd)/starchaser/starchaser:
-python -m pip install virtualenv
-python -m virtualenv pymote_env_linux
-source pymote_env_linux/bin/activate
-dos2unix requirements.txt
-pip install -r requirements.txt
+start the Ubuntu and ssh in
+https://linuxhint.com/how-to-install-postgresql-on-aws-ec2/
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install postgresql -y
 
-echo '
+On add port 5432 to EC2 inbound network security channel
+
+--------------------
+good instructions at: 
+   https://medium.com/ruralscript/install-and-setuppostgresql-on-ubuntu-amazon-ec2-5d1af79b4fca
+
+Access the postgres account on your server by typing:
+
+$ sudo -i -u postgres
+Now you can immediately access Postgres prompt by typing:
+
+$ psql
+postgres=#
+You will be logged in and able to interact with the Postgres database management system.
+
+To list the databases:
+
+postgres=# \l
+You can exit Postgres prompt by typing:
+
+postgres=# \q
+------------------------
+Change the password for postgres user
+Connect to the server using psql:
+
+postgres=# \password
+Enter new password:
+Enter it again:
+postgres=#
+----------------------------
+Allow remote connections to PostgreSQL database server
+90  cd /etc/postgresql/12/main/
+sudo vi pg_hba.conf
+host  all        all        0.0.0.0/0        md5
+
+sudo vi postgresql.conf
+listen_addresses = '*'                # what IP address(es) to listen on;
+
+81  sudo systemctl restart postgresql.service
+
+at this point you should be able to use PGADMIN to connect 
+
+--------------------------
+for ubuntu on EC2... need venv...
+assuming here python is 3.8...
+
+205  sudo apt install python3.8-venv
+206  python -m venv venv
+
+--------------------------
+
+#export PYTHONPATH=$(pwd):$(pwd)/starchaser:$(pwd)/starchaser/starchaser:
+#python -m pip install virtualenv
+#python -m virtualenv pymote_env_linux
+#source pymote_env_linux/bin/activate
+#dos2unix requirements.txt
+#pip install -r requirements.txt
+
+----------------------------
+200  python --version
+205  sudo apt install python3.8-venv
+206  python -m venv venv
+210  source venv/bin/activate
+213  sudo apt install dos2unix
+214  dos2unix requirements.txt
+220  pip install -r requirements.txt
+
+----------------------------
 
 This is setup script and instructions to deploy StarChaser on AWS including the PostgreSQL data subset
 It is intended to be run on the EC2  
+
+242  export PYTHONPATH=$(pwd):$(pwd)/starchaser:$(pwd)/starchaser/starchaser:
+
+252  vi  starchaser/passwords.yml (copy from passwords.yml.EXAMPLE)
+
+257  sudo -u postgres createdb plasticc
+sudo -u postgres psql
+\l
+
 
 To initialize the database:
  python starchaser/manage.py makemigrations
